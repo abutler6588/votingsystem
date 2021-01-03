f7a76ecc47a78910a53016ffa55bb169b0d87226
abutler6588 <abutler6588@gmail.com>
2021-01-03 17:01:12 -0600

    Finalizing project - Installed a logger. Created a voter.html file, created a file to present voting with the same exact name. Setup logger configuration by using Springboot built in logging capabilities, implementing a form of caching.

--- src/main/java/com/myproject/votingsystem/controller/VotingController.java
+++ src/main/java/com/myproject/votingsystem/controller/VotingController.java
@@ -2,6 +2,9 @@ package com.myproject.votingsystem.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
+import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +20,8 @@ import com.myproject.votingsystem.repositories.CitizenRepo;
 @Controller
 public class VotingController {
 	
+	public final Logger logger = Logger.getLogger(VotingController.class);
+	
 	@Autowired
 	CitizenRepo citizenRepo;
 	
@@ -26,17 +31,25 @@ public class VotingController {
 	//landing page directs to vote.html 
 	@RequestMapping("/")
 	public String goToVote() {
+		logger.info("Returning vote.html file");
 		return "vote.html";
 	}
 	
 	//then enter our citizen name to login
 	//checking that citizen/name  has not yet already voted
 	@RequestMapping("/doLogin") 
-	public String doLogin(@RequestParam String name, Model model) {
-				//search database for the citizen
+	public String doLogin(@RequestParam String name, Model model, HttpSession session) {
+		
+		logger.info("getting citizen from database");
+		
+		//search database for the citizen
 		Citizen citizen = citizenRepo.findByName(name);
+		logger.info("putting citizen into session");
+		//insert the citizen name during HTTPSession into session
+		session.setAttribute("citizen", citizen);
 				//if citizen has voted returns true, then perform return statement
 		if (!citizen.getHasVoted()) {
+			logger.info("putting candidates into model");
 			List<Candidate> candidates = candidateRepo.findAll();
 			model.addAttribute("candidates", candidates);
 			return "/performVoted.html";
@@ -46,14 +59,28 @@ public class VotingController {
 	}
 	
 	@RequestMapping("/voteFor")
-	public String voteFor(@RequestParam Long id) {
+	public String voteFor(@RequestParam Long id, HttpSession session) {
+		//check to see if citizen has already voted
+
+		Citizen citizen = (Citizen)session.getAttribute("citizen");
 		
+	
+		//if citzien.getHasVoted is false
+		if(!citizen.getHasVoted()) {
+			//then set citizen.setHasVoted to true
+		citizen.setHasVoted(true);
+			//then allow citizen to vote, works based upon the ID given for the citizen
 		Candidate c = candidateRepo.findById((long)id);
+		logger.info("voting for candidate" + c.getName());
 		c.setNumberOfVotes(c.getNumberOfVotes()+1);
+		
 		candidateRepo.save(c);
+		citizenRepo.save(citizen);
 		
 		return "voted.html";
+		}
 		
+		return "alreadyVoted.html";
 	}
 	}
 	
